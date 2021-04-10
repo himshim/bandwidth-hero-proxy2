7a8111a6df6d3405b1cba71bfc0a9d4d70175f35
Aditya Gupta <adityag.ug19.cs@nitp.ac.in>
2021-04-11 02:29:57 +0530

    Fix deployment
    The problem was... I used "??", and it was supported on my pc, so ntl dev worked, but maybe AWS uses older node that doesn't support it, i had to change to ||

--- functions/index.js
+++ functions/index.js
@@ -6,8 +6,6 @@ const compress = require("../util/compress");
 const DEFAULT_QUALITY = 40;
 
 exports.handler = async (event, context) => {
-    console.error("Reached here...");
-    console.log(event);
     let { url } = event.queryStringParameters;
     const { jpeg, bw, l } = event.queryStringParameters;
 
@@ -34,65 +32,70 @@ exports.handler = async (event, context) => {
     const quality = parseInt(l, 10) || DEFAULT_QUALITY;
 
     console.log("Fetching...", url);
-    const { data, type: originType } = await fetch(url, {
-        headers: {
-            ...pick(event.headers, ['cookie', 'dnt', 'referer']),
-            'user-agent': 'Bandwidth-Hero Compressor',
-            'x-forwarded-for': event.headers['x-forwarded-for'] || event.ip,
-            via: '1.1 bandwidth-hero'
-        }
-        // timeout: 10000,
-        // maxRedirects: 5,
-        // encoding: null,
-        // strictSSL: false,
-        // gzip: true,
-        // jar: true
-    }).then(async res => {
-        if (!res.ok){
-            return {
-                statusCode: res.status ?? 302
-            }
-        }
-
-        return {
-            data: await res.text(),
-            type: res.headers.get("content-type") || ""
-        }
-    })
-
-    const originSize = data.length;
-    console.log({originType, originSize});
-
-    if (shouldCompress(originType, originSize, webp)) {
-        const {err, output, headers} = await compress(data, webp, grayscale, quality, originSize);   // compress
-
-        if(err) {
-            console.log("Conversion failed: ", url);
-            return {
-                statusCode: 500,
-                body: err.message ?? ""
-            }
-        }
-
-        console.log(`Compressed from ${originSize}, to ${output.length}`);
-        return {
-            statusCode: 200,
-            body: output,
+    try {
+        const { data, type: originType } = await fetch(url, {
             headers: {
-                "content-encoding": "identity",
-                ...event.headers,
-                ...headers
+                ...pick(event.headers, ['cookie', 'dnt', 'referer']),
+                'user-agent': 'Bandwidth-Hero Compressor',
+                'x-forwarded-for': event.headers['x-forwarded-for'] || event.ip,
+                via: '1.1 bandwidth-hero'
+            }
+            // timeout: 10000,
+            // maxRedirects: 5,
+            // encoding: null,
+            // strictSSL: false,
+            // gzip: true,
+            // jar: true
+        }).then(async res => {
+            if (!res.ok) {
+                return {
+                    statusCode: res.status || 302
+                }
+            }
+
+            return {
+                data: await res.text(),
+                type: res.headers.get("content-type") || ""
+            }
+        })
+
+        const originSize = data.length;
+        console.log({ originType, originSize });
+
+        if (shouldCompress(originType, originSize, webp)) {
+            const { err, output, headers } = await compress(data, webp, grayscale, quality, originSize);   // compress
+
+            if (err) {
+                console.log("Conversion failed: ", url);
+                throw err;
+            }
+
+            console.log(`Compressed from ${originSize}, to ${output.length}`);
+            return {
+                statusCode: 200,
+                body: output,
+                headers: {
+                    "content-encoding": "identity",
+                    ...event.headers,
+                    ...headers
+                }
+            }
+        } else {
+            console.log("Bypassing request...");
+            return {    // bypass
+                statusCode: 200,
+                body: data,
+                headers: {
+                    "x-proxy-bypass": '1',
+                    "content-length": data.length
+                }
             }
         }
-    } else {
-        console.log("Bypassing request...");
-        return {    // bypass
-            statusCode: 200,
-            body: data,
-            headers: {
-                "x-proxy-bypass": '1',
-                "content-length": data.length
-            }
+    } catch (err) {
+        console.error(err);
+        return {
+            statusCode: 500,
+            body: err.message || ""
         }
     }
 }
